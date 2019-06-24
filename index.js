d82adb454a1fbdb45f49e84948879934886aa56e
Stefan Scepanovic <scepa91@icloud.com>
2019-06-24 17:46:04 -0500

    stefan

--- index.js
+++ index.js
@@ -1,71 +1,80 @@
 // Define a variable to use when we need to set the characters image src
-const ASSET_ROOT = './assets/green-character'
+const ASSET_ROOT = "./assets/green-character";
 
 // Create an img element to represent the character
-const character = document.createElement('img')
+const character = document.createElement("img");
 
 // Make the character a little bigger
-character.style.width = '75px'
+character.style.width = "110px";
 
 // Position the character absolutely in the lower left corner of the screen
-character.style.position = 'absolute'
-character.style.left = '0px'
-character.style.bottom = '0px'
+character.style.position = "absolute";
+character.style.left = "0px";
+character.style.bottom = "0px";
 
 // Set the src of the img so the browser knows what to show
-character.src = `${ASSET_ROOT}/static.gif`
+character.src = `${ASSET_ROOT}/static.gif`;
 
 // Add the character image to the page
-document.body.append(character)
+document.body.append(character);
 
 // Define a variable to represent the direction our character is moving
-let direction = null
+let direction = null;
 
 // Define a variable to reperesent the speed of our character
-let speed = 5
+let speed = 1;
 
 // Have someone at your table do some research on  setInterval- what is it doing?
-setInterval(function(){
+setInterval(function() {
+  // character.style.left / bottom are both strings: "0px"
+  // If we want to do some arithmatic, we'll need to parse them into integers:
+  const left = parseInt(character.style.left);
+  const bottom = parseInt(character.style.bottom);
 
-    // character.style.left / bottom are both strings: "0px"
-    // If we want to do some arithmatic, we'll need to parse them into integers:
-    const left = parseInt(character.style.left)
-    const bottom = parseInt(character.style.bottom)
+  // If the character is moving right, the distance between him and the left side of the screen should increase
+  // What is speed?
+  if (direction == "right") {
+    character.style.left = `${left + speed}px`;
+  }
+  if (direction == "left") {
+    character.style.left = `${left - speed}px`;
+  }
+  if (direction == "up") {
+    character.style.bottom = `${bottom + speed}px`;
+  }
+  if (direction == "bottom") {
+    character.style.bottom = `${bottom - speed}px`;
+  }
 
-    // If the character is moving right, the distance between him and the left side of the screen should increase
-    // What is speed?
-    if(direction == 'right'){
-        character.style.left = `${left+speed}px`
-    }
-
-    // Account for other directions here:
-
-}, 20) // <- What is this number?
-
-// When we want to start walking a given direction, let's change: 
+  // Account for other directions here:
+  // <- What is this number?
+}, 20);
+// When we want to start walking a given direction, let's change:
 //  1. The characters gif.
 //  2. The value of the direction variable. How will this effect the setInterval loop above?
-function walkRight(){
-    character.src = `${ASSET_ROOT}/walkright.gif`
-    direction = 'right'
+function walkRight() {
+  character.src = `${ASSET_ROOT}/walkright.gif`;
+  direction = "right";
 }
-
 
 // Finish the functions below:
 
-function walkLeft(){
-
+function walkLeft() {
+  character.src = `${ASSET_ROOT}/walkleft.gif`;
+  direction = "left";
 }
 
-function walkUp(){
-
+function walkUp() {
+  character.src = `${ASSET_ROOT}/walkup.gif`;
+  direction = "up";
 }
 
-function walkDown(){
-
+function walkDown() {
+  character.src = `${ASSET_ROOT}/walkdown.gif`;
+  direction = "bottom";
 }
 
-function stop(){
-    character.src = `${ASSET_ROOT}/static.gif`
-    direction = null
+function stop() {
+  character.src = `${ASSET_ROOT}/static.gif`;
+  direction = null;
 }
